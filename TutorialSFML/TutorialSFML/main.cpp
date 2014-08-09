f42a74cde63a5e27500c3cce144e2a53dcdf79c7
Sonar Systems <admin@sonarsystems.co.uk>
2014-08-09 10:52:33 +0100

    Window Events

---
+++ TutorialSFML/TutorialSFML/main.cpp
@@ -0,0 +1,42 @@
+#include "SFML/Graphics.hpp"
+#include <iostream>
+
+int main()
+{
+	sf::RenderWindow window( sf::VideoMode( 600, 600 ), "SFML WORK!" );
+
+	while ( window.isOpen( ) )
+	{
+		sf::Event event;
+
+		while ( window.pollEvent( event ) )
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+
+				break;
+
+			case sf::Event::Resized:
+				std::cout << event.size.width << " : " << event.size.height << std::endl;
+
+				break;
+
+			case sf::Event::LostFocus:
+				std::cout << "Lost Focus" << std::endl;
+
+				break;
+
+			case sf::Event::GainedFocus:
+				std::cout << "Gained focus" << std::endl;
+
+				break;
+			}
+		}
+
+		window.clear( );
+
+		window.display( );
+	}
+}
